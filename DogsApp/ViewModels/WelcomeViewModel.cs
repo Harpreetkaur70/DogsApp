22987040424a62a9dbb031872ede69a1580de493
Harpreet <harpreetkaur11379@gmail.com>
2022-02-08 17:31:11 +0530

    Created Button
    Created Open Google Button ,Added Command in Welcome Page, Added OpenGoogle Method in WelcomeViewModel ,Open Google page after clicked on Open Google Button

--- DogsApp/ViewModels/WelcomeViewModel.cs
+++ DogsApp/ViewModels/WelcomeViewModel.cs
@@ -1,15 +1,25 @@
 ﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
 
 namespace DogsApp.ViewModels
 {
     class WelcomeViewModel :BaseViewModel
     {
+        public ICommand OpenGoogleCommand { get; }
         public WelcomeViewModel()
         {
             Title = "Welcome";
+            OpenGoogleCommand = new AsyncCommand(OpenGoogle);
+        }
+        private async Task OpenGoogle()
+        {
+            await Browser.OpenAsync("https://www.google.com");
         }
     }
 }
