a2ecc4caa7d424f859a049a5ec8e68a6d546e4a5
Harpreet <harpreetkaur11379@gmail.com>
2022-02-08 17:17:09 +0530

    WelcomeViewModel Created with Binding
    Welcome Page created with its View Model ,Binding Context added linking done, Base Class Added for View Model

---
+++ DogsApp/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogsApp.ViewModels
+{
+    class WelcomeViewModel :BaseViewModel
+    {
+        public WelcomeViewModel()
+        {
+            Title = "Welcome";
+        }
+    }
+}
